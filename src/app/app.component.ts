5badd2ca59b6ecf8956d0182022b45c79cc635ce
pouyan Kheradmand <pooyan.kheradmand@gmail.com>
2021-07-16 10:28:47 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'FE21-CR06-Pouyan';
+}
